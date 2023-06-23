c708c5842009ace0d04219fb73cb5dab9bfd0a1c
Samarth <samsaxena835@gmail.com>
2023-06-23 11:04:16 +0530

    firstbranchcommit

--- middleware/auth.js
+++ middleware/auth.js
@@ -9,7 +9,7 @@ const isAuthentiated = async(req,res,next)=>{
         const token = req.headers.authorization.split(' ')[1]
         console.log(token)
         if(!token){
-            const error = Error('Login First to access the the link');error.code=401;return next(error)
+            const error = Error('Login First to get the link');error.code=401;return next(error)
         }
         const decoded = jwt.verify(token,'DEDEJIDSDSFEFDSC84511812CDC');
         req.number = {mobile_number:decoded.id}
